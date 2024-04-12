5982e445e778d41de3f84057485f5e0f26a58339
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-04-12 22:28:33 +0700

    chore: learn variable

---
+++ src/main/kotlin/Variable.kt
@@ -0,0 +1,13 @@
+
+fun main(){
+    // Variable tanpa deklarasi tipe data
+    val firstName = "Maulana Sodiqin"
+
+    // Varible dengan deklarasi tipe data
+    val lastName: String = "Sodiqin"
+
+    val age: Int = 10;
+
+
+    print(firstName)
+}
